8101a513a678f6618a56225d5d6034aba575de46
Mati <maticristovao@hotmail.com>
2023-05-15 17:54:07 -0300

    dockerfile for Render

--- Portfolio/src/main/java/com/project/Portfolio/controller/EmployTypeController.java
+++ Portfolio/src/main/java/com/project/Portfolio/controller/EmployTypeController.java
@@ -6,14 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employ-type")
-@CrossOrigin(origins = "http://localhost:4200")
 public class EmployTypeController {
 
     @Autowired
--- Portfolio/src/main/java/com/project/Portfolio/controller/ProvinceController.java
+++ Portfolio/src/main/java/com/project/Portfolio/controller/ProvinceController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/province")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProvinceController {
 
     @Autowired
@@ -25,7 +23,7 @@ public class ProvinceController {
         List<Province> provList = provServ.getProvinces();
         return new ResponseEntity<>(provList, HttpStatus.OK);
     }
-    
+
     @GetMapping("/country/{id}")
     public ResponseEntity<List<Province>> getProvincesByCountryId(@PathVariable Long id) {
         List<Province> provList = provServ.getProvincesByCountryId(id);
