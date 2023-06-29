f6ddebdc21165e5cfc29cd7876b1d3272dd98ee8
Mati <maticristovao@hotmail.com>
2023-06-29 17:04:51 -0300

    institution update fix

--- Portfolio/src/main/java/com/project/Portfolio/controller/InstitutionController.java
+++ Portfolio/src/main/java/com/project/Portfolio/controller/InstitutionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,7 +34,7 @@ public class InstitutionController {
         return new ResponseEntity<>(newInst, HttpStatus.OK);
     }
     
-    @PostMapping("/update")
+    @PutMapping("/update")
     public ResponseEntity<Institution> updateInstitution(@RequestBody Institution inst) {
         Institution updatedInst = instServ.addInstitution(inst);
         return new ResponseEntity<>(updatedInst, HttpStatus.OK);
